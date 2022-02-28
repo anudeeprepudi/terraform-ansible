30a65e3a60b92b43f6665b743a8b6734f458d16c
anudeeprepudi <95208288+anudeeprepudi@users.noreply.github.com>
2022-02-28 17:33:37 -0500

    Update install-ansible-terraform.sh

--- install-ansible-terraform.sh
+++ install-ansible-terraform.sh
@@ -3,5 +3,5 @@ sudo apt -y update
 sudo apt -y install ansible
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt install terraform
+sudo apt install terrafrom
 
